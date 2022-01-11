77482c3fd154d6eddb629a67eafaff6aaa098890
spectatorMrZ <13959374+SpectatorNan@users.noreply.github.com>
2022-01-11 18:51:45 +0800

    temp

--- gormx/sqlconn.go
+++ gormx/sqlconn.go
@@ -2,29 +2,29 @@ package gormx
 
 import (
 	"database/sql"
-	"errors"
 	"github.com/tal-tech/go-zero/core/breaker"
-	"github.com/tal-tech/go-zero/core/syncx"
+	"github.com/tal-tech/go-zero/core/logx"
+	"github.com/tal-tech/go-zero/core/timex"
 	"gorm.io/gorm"
-	"io"
-	"sync"
-	"time"
-	"gorm.io/driver/postgres"
 )
 
 var ErrNotFound = gorm.ErrRecordNotFound
 
 type (
-
 	Session interface {
-		Exec(queryDB *gorm.DB)
+		First(v interface{}) error
+		QueryRow(v interface{}) error
+		QueryRows(v interface{}) error
 	}
 
 	SqlConn interface {
-
+		Session
 		RawDB() (*sql.DB, error)
 		Transact(func(session Session) error) error
 	}
+
+	// SqlOption defines the method to customize a sql connection.
+	SqlOption func(*commonSqlConn)
 
 	// thread-safe
 	// Because CORBA doesn't support PREPARE, so we need to combine the
@@ -37,13 +37,13 @@ type (
 		accept   func(error) bool
 	}
 
-	connProvider func() (*sql.DB, error)
+	connProvider func() (*gorm.DB, error)
 )
 
 // NewSqlConn returns a SqlConn with given driver name and datasource.
-func NewSqlConn(driverName, datasource string, opts ...SqlOption) SqlConn {
+func NewSqlConn(driverName DriverType, datasource string, opts ...SqlOption) SqlConn {
 	conn := &commonSqlConn{
-		connProv: func() (*sql.DB, error) {
+		connProv: func() (*gorm.DB, error) {
 			return getSqlConn(driverName, datasource)
 		},
 		onError: func(err error) {
@@ -59,120 +59,73 @@ func NewSqlConn(driverName, datasource string, opts ...SqlOption) SqlConn {
 	return conn
 }
 
-/* file: sqlmanager */
-type DriverType int
-const (
-	MysqlDriver DriverType = iota
-	PostgresDriver
-)
-
-const (
-	maxIdleConns = 64
-	maxOpenConns = 64
-	maxLifetime  = time.Minute
-)
-
-var connManager = syncx.NewResourceManager()
-
-type pingedDB struct {
-	*gorm.DB
-	once sync.Once
-}
-func getCachedSqlConn(driver DriverType, server string) (*pingedDB, error) {
-	val, err := connManager.GetResource(server, func() (io.Closer, error) {
-		conn, err := newDBConnection(driver, server)
-		if err != nil {
-			return nil, err
+// NewSqlConnFromDB returns a SqlConn with the given sql.DB.
+// Use it with caution, it's provided for other ORM to interact with.
+func NewSqlConnFromDB(db *sql.DB, opts ...SqlOption) SqlConn {
+	// wait implement
+	/*
+		conn := &commonSqlConn{
+			connProv: func() (*sql.DB, error) {
+				return db, nil
+			},
+			onError: func(err error) {
+				logx.Errorf("Error on getting sql instance: %v", err)
+			},
+			beginTx: begin,
+			brk:     breaker.NewBreaker(),
+		}
+		for _, opt := range opts {
+			opt(conn)
 		}
 
-		return &pingedDB{
-			DB: conn,
-		}, nil
+		return conn
+	*/
+	return nil
+}
+
+func (db *commonSqlConn) acceptable(err error) bool {
+	ok := err == nil || err == gorm.ErrRecordNotFound || err == gorm.errtr
+	if db.accept == nil {
+		return ok
+	}
+
+	return ok || db.accept(err)
+}
+
+func (db *commonSqlConn) queryRows(engine func(db *gorm.DB) error, dest interface{}) error {
+	var qerr error
+	return db.brk.DoWithAcceptable(func() error {
+		conn, err := db.connProv()
+		if err != nil {
+			db.onError(err)
+			return err
+		}
+		return db.exec(func() error {
+			qerr = conn.Find(&dest).Error
+			return qerr
+		})
+		//return query(conn, func(rows *sql.Rows) error {
+		//	qerr = scanner(rows)
+		//	return qerr
+		//}, q, args...)
+	}, func(err error) bool {
+		return qerr == err || db.acceptable(err)
 	})
-	if err != nil {
-		return nil, err
-	}
-
-	return val.(*pingedDB), nil
-}
-func getSqlConn(driver DriverType, server string) (*gorm.DB, error) {
-	pdb, err := getCachedSqlConn(driver, server)
-	if err != nil {
-		return nil, err
-	}
-// gorm has auto ping
-	//pdb.once.Do(func() {
-	//	err = pdb.Ping()
-	//})
-	if err != nil {
-		return nil, err
-	}
-
-	return pdb.DB, nil
-}
-func newMysqlConnect(datasource string, dbCfg gorm.Config) (*gorm.DB, error) {
-	db, err := gorm.Open(postgres.Open(datasource), &dbCfg)
-	if err != nil {
-		return nil, err
-	}
-
-	conn, err := db.DB()
-
-	// we need to do this until the issue https://github.com/golang/go/issues/9851 get fixed
-	// discussed here https://github.com/go-sql-driver/mysql/issues/257
-	// if the discussed SetMaxIdleTimeout methods added, we'll change this behavior
-	// 8 means we can't have more than 8 goroutines to concurrently access the same database.
-	conn.SetMaxIdleConns(maxIdleConns)
-	conn.SetMaxOpenConns(maxOpenConns)
-	conn.SetConnMaxLifetime(maxLifetime)
-
-	return db, nil
-}
-func newPostgresConnect(datasource string, dbCfg gorm.Config) (*gorm.DB, error) {
-	db, err := gorm.Open(postgres.Open(datasource), &dbCfg)
-	if err != nil {
-		return nil, err
-	}
-
-	conn, err := db.DB()
-
-	// we need to do this until the issue https://github.com/golang/go/issues/9851 get fixed
-	// discussed here https://github.com/go-sql-driver/mysql/issues/257
-	// if the discussed SetMaxIdleTimeout methods added, we'll change this behavior
-	// 8 means we can't have more than 8 goroutines to concurrently access the same database.
-	conn.SetMaxIdleConns(maxIdleConns)
-	conn.SetMaxOpenConns(maxOpenConns)
-	conn.SetConnMaxLifetime(maxLifetime)
-
-	return db, nil
-}
-func newDBConnection(driver DriverType, server string) (*gorm.DB, error) {
-	switch driver {
-	case MysqlDriver:
-		return newMysqlConnect(server, gorm.Config{})
-	case PostgresDriver:
-		return newPostgresConnect(server, gorm.Config{})
-	default:
-		return nil, errors.New("un support db driver")
-	}
 }
 
-/*
-func newDBConnection(driverName, datasource string) (*sql.DB, error) {
+func (db *commonSqlConn) exec(execFn func() error) error {
 
-	conn, err := sql.Open(driverName, datasource)
-	if err != nil {
-		return nil, err
+	startTime := timex.Now()
+	err := execFn()
+	duration := timex.Since(startTime)
+	if duration > slowThreshold.Load() {
+		logx.WithDuration(duration).Slowf("[SQL] query: slowcall - %s", stmt)
+	} else {
+		logx.WithDuration(duration).Infof("sql query: %s", stmt)
 	}
-
-	// we need to do this until the issue https://github.com/golang/go/issues/9851 get fixed
-	// discussed here https://github.com/go-sql-driver/mysql/issues/257
-	// if the discussed SetMaxIdleTimeout methods added, we'll change this behavior
-	// 8 means we can't have more than 8 goroutines to concurrently access the same database.
-	conn.SetMaxIdleConns(maxIdleConns)
-	conn.SetMaxOpenConns(maxOpenConns)
-	conn.SetConnMaxLifetime(maxLifetime)
-
-	return conn, nil
+	if err != nil {
+		logSqlError(stmt, err)
+		return err
+	}
+	return nil
 }
-*/
--- gormx/tx.go
+++ gormx/tx.go
@@ -1,17 +1,33 @@
 package gormx
 
-import "database/sql"
+import (
+	"gorm.io/gorm"
+)
 
 type (
-	beginnable func(*sql.DB) (trans, error)
+	beginnable func(*gorm.DB) (trans, error)
 
 	trans interface {
 		Session
-		Commit() error
-		Rollback() error
+		Commit() *gorm.DB
+		Rollback() *gorm.DB
 	}
 
 	txSession struct {
-		*sql.Tx
+		Tx *gorm.DB
 	}
 )
+
+func (tx txSession) Commit() *gorm.DB {
+	return tx.Tx.Commit()
+}
+func (tx txSession) Rollback() *gorm.DB {
+	return tx.Tx.Rollback()
+}
+func begin(db *gorm.DB) (trans, error) {
+	tx := db.Begin()
+
+	return txSession{
+		Tx: tx,
+	}, nil
+}
