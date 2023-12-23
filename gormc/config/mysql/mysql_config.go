6821681b345b90c7693e49029ad0e4c25a62421d
spectatorMrZ <zhuchunan001@sina.com>
2023-12-24 00:01:14 +0800

    style： gofmt

--- gormc/config/mysql/mysql_config.go
+++ gormc/config/mysql/mysql_config.go
@@ -12,17 +12,17 @@ import (
 )
 
 type Mysql struct {
-	Path          string // 服务器地址
-	Port          int    `json:",default=3306"`                                               // 端口
-	Config        string `json:",default=charset%3Dutf8mb4%26parseTime%3Dtrue%26loc%3DLocal"` // 高级配置
-	Dbname        string // 数据库名
-	Username      string // 数据库用户名
-	Password      string // 数据库密码
-	MaxIdleConns  int    `json:",default=10"`                               // 空闲中的最大连接数
-	MaxOpenConns  int    `json:",default=10"`                               // 打开到数据库的最大连接数
-	LogMode       string `json:",default=dev,options=dev|test|prod|silent"`
+	Path         string // 服务器地址
+	Port         int    `json:",default=3306"`                                               // 端口
+	Config       string `json:",default=charset%3Dutf8mb4%26parseTime%3Dtrue%26loc%3DLocal"` // 高级配置
+	Dbname       string // 数据库名
+	Username     string // 数据库用户名
+	Password     string // 数据库密码
+	MaxIdleConns int    `json:",default=10"` // 空闲中的最大连接数
+	MaxOpenConns int    `json:",default=10"` // 打开到数据库的最大连接数
+	LogMode      string `json:",default=dev,options=dev|test|prod|silent"`
 	//LogZap        bool   // 是否通过zap写入日志文件
-	SlowThreshold int64  `json:",default=1000"`
+	SlowThreshold int64 `json:",default=1000"`
 }
 
 func (m *Mysql) Dsn() string {
@@ -78,9 +78,9 @@ func ConnectWithConfig(m Mysql, cfg *gorm.Config) (*gorm.DB, error) {
 		return nil, err
 	}
 
-		sqldb, _ := db.DB()
-		sqldb.SetMaxIdleConns(m.MaxIdleConns)
-		sqldb.SetMaxOpenConns(m.MaxOpenConns)
-		return db, nil
+	sqldb, _ := db.DB()
+	sqldb.SetMaxIdleConns(m.MaxIdleConns)
+	sqldb.SetMaxOpenConns(m.MaxOpenConns)
+	return db, nil
 
 }
