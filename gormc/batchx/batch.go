d6d17a8bebb3b896097e061d7fbc060d01a0156f
spectatorNan <spectatornan@gmail.com>
2024-07-25 02:32:53 +0800

    feat: batch execute with all cache keys

---
+++ gormc/batchx/batch.go
@@ -0,0 +1,35 @@
+package batchx
+
+import (
+	"context"
+	"github.com/SpectatorNan/gorm-zero/gormc"
+	"gorm.io/gorm"
+)
+
+type BatchExecModel[DBModel any] interface {
+	CustomCacheKeys(data *DBModel) []string
+	ExecCtx(ctx context.Context, execCtx gormc.ExecCtxFn, keys ...string) error
+}
+
+func BatchExecCtx[DBModel any, Model BatchExecModel[DBModel]](ctx context.Context, model Model, olds []DBModel, exec func(db *gorm.DB) error) error {
+	if len(olds) == 0 {
+		return nil
+	}
+	cacheKeys := getCacheKeysByMultiData(model, olds)
+
+	err := model.ExecCtx(ctx, func(conn *gorm.DB) error {
+		return exec(conn)
+	}, cacheKeys...)
+	return err
+}
+
+func getCacheKeysByMultiData[DBModel any, Model BatchExecModel[DBModel]](m Model, data []DBModel) []string {
+	if len(data) == 0 {
+		return []string{}
+	}
+	var keys []string
+	for _, v := range data {
+		keys = append(keys, m.CustomCacheKeys(&v)...)
+	}
+	return keys
+}
