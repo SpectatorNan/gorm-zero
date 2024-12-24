f492fed9cce32fcda0b317f795f7736a5c623a9c
spectatorMrZ <zhuchunan001@sina.com>
2024-12-25 03:19:55 +0800

    feat: pagex time range support customer column

--- gormc/pagex/timeRange.go
+++ gormc/pagex/timeRange.go
@@ -34,6 +34,30 @@ func TimeRangeByTable(tableName string, startTime, endTime *time.Time) func(db *
 	}
 }
 
+func TimeRangeByColumn(column string, startTime, endTime *time.Time) func(db *gorm.DB) *gorm.DB {
+	return TimeRangeByTableNameColumn("", column, startTime, endTime)
+}
+
+func TimeRangeByTableNameColumn(tableName string, column string, startTime, endTime *time.Time) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		if startTime != nil {
+			sql := fmt.Sprintf("%s >= ?", column)
+			if len(tableName) > 0 {
+				sql = tableName + "." + sql
+			}
+			db = db.Where(sql, *startTime)
+		}
+		if endTime != nil {
+			sql := fmt.Sprintf("%s <= ?", column)
+			if len(tableName) > 0 {
+				sql = tableName + "." + sql
+			}
+			db = db.Where(sql, *endTime)
+		}
+		return db
+	}
+}
+
 func Paginate(page *ListReq) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		if page == nil {
